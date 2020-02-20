6d636d58d49933329133dc5106417a5f029ace0d
Tomasz Czajka <Tomasz.Czajka@unit4.com>
2020-02-20 12:54:31 +0100

    initial working version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArchwizardModule } from 'angular-archwizard';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ArchwizardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
